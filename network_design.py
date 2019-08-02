64ea2a277efaad6fa09a034534df134d9db1d084
Kyle Pearson <pearsonk@email.arizona.edu>
2019-08-01 17:08:43 -0700

    neural network

--- network_design.py
+++ network_design.py
@@ -1,13 +1,3 @@
-'''
-Inputs: 
-    2D spectral time-series
-    1D white light
-    1D template
-    1D biased spectrum
-
-Output: 
-    1D unbiased spectrum 
-'''
 import pickle
 import argparse
 
